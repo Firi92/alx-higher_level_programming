a03cfc08b6fcde8730dc2948e441ec21d7b56761
ibifiri <ibifiri4ever@gmail.com>
2023-12-20 05:42:38 -0800

    python exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -7,10 +7,3 @@ def safe_print_integer(value):
         return (True)
     except (TypeError, ValueError):
         return (False)
-
-2-main.py
-
-
-vi 2-safe_print_list_integers.py
-
-#!/usr/bin/python3
